d690622599eccce0b1f886f41d341b688ae803a3
cleisonsantos <css.cleison@outlook.com>
2024-02-09 03:36:55 -0300

    variables

---
+++ variables.lua
@@ -0,0 +1,13 @@
+function setup()
+    a = 3
+    b = 8
+    c = a + b
+    text = 'My name'
+    isCorrect = true
+    print(a)
+    print(b)
+    print(c)
+    print(text, isCorrect)
+end
+
+setup()
